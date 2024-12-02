abea146a9904b6114094001a098c9ee6ced7cd3f
Iryna Oliinyk <arairishka2@gmail.com>
2024-12-02 23:33:35 +0100

    start

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,7 @@ import {
   loginUser,
   logoutUser,
   refreshUsersSession,
+  requestResetToken,
 } from '../services/auth.js';
 
 import { ONE_DAY } from '../constants/index.js';
@@ -72,5 +73,14 @@ export const refreshUserSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
   });
 };
