5ee50aa8991b0952820e3da70e249de763bc7a0c
Iryna Oliinyk <arairishka2@gmail.com>
2024-12-17 15:41:17 +0100

    update 5.1

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,9 +4,23 @@ dotenv.config();
 
 export function env(name, defaultValue) {
   const value = process.env[name];
+
   if (value) return value;
 
   if (defaultValue) return defaultValue;
 
-  throw new Error(`Missing:process.env['${name}']`);
+  throw new Error(`Missing: process.env['${name}'].`);
 }
+
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// export function env(name, defaultValue) {
+//   const value = process.env[name];
+//   if (value) return value;
+
+//   if (defaultValue) return defaultValue;
+
+//   throw new Error(`Missing:process.env['${name}']`);
+// }
